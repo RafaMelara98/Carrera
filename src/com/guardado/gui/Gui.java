657242818862a16d43ec54f0c58cb7ceeafc342d
LN710Q <LN710Q@DEILABO1-PC30>
2018-06-04 12:51:23 -0600

    Carrera

---
+++ src/com/guardado/gui/Gui.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guardado.gui;
+
+import com.guardado.threads.AnimalThread;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import static sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte0.runnable;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Gui extends JFrame {
+
+    private JLabel[] labels;
+    private JButton reinicio;
+    private JButton inicio;
+    private String[] nombres = {"canguro", "tortuga", "dragon"};
+
+    public Gui() {
+        super("carrera de animales");
+        initialComponents();
+
+    }
+
+    private void initialComponents() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        labels = new JLabel[3];
+        inicio = new JButton("incio");
+        reinicio = new JButton("reinicio");
+        Container container = getContentPane();
+
+        for (int i = 0; i < 3; i++) {
+            labels[i] = new JLabel();
+            labels[i].setIcon(new ImageIcon(getClass().getResource(nombres[i] + ".gif")));
+            labels[i].setBounds(10, (i * 220) + 10, 200, 200);
+            container.add(labels[i]);
+        }
+        inicio.setBounds(10, 0, 100, 30);
+        container.add(inicio);
+
+        inicio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AnimalThread canguro = new AnimalThread("canguro", labels[0].getX(), labels[0].getY(), 510, labels[0]);
+                AnimalThread tortuga = new AnimalThread("tortuga", labels[1].getX(), labels[1].getY(), 510, labels[1]);
+                AnimalThread dragon = new AnimalThread("dragon", labels[2].getX(), labels[2].getY(), 510, labels[2]);
+                canguro.start();
+                tortuga.start();
+                dragon.start();
+            }
+        });
+        setSize(700, 650);
+
+        reinicio.setBounds(410, 0, 100, 30);
+        container.add(reinicio);
+
+        reinicio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labels[0].setBounds(10, (0 * 220) + 10, 200, 200);
+                labels[1].setBounds(10, (1 * 220) + 10, 200, 200);
+                labels[2].setBounds(10, (2 * 220) + 10, 200, 200);
+                AnimalThread canguro = new AnimalThread("canguro", labels[0].getX(), labels[0].getY(), 510, labels[0]);
+                AnimalThread tortuga = new AnimalThread("tortuga", labels[1].getX(), labels[1].getY(), 510, labels[1]);
+                AnimalThread dragon = new AnimalThread("dragon", labels[2].getX(), labels[2].getY(), 510, labels[2]);
+             
+
+            }
+        });
+        setSize(700, 650);
+
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Gui().setVisible(true);
+            }
+
+        });
+    }
+}
